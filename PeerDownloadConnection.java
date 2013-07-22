90437ca90ef90f9247221266d483e83d43b821fd
A L <padtherays@yahoo.com>
2013-07-22 19:38:05 -0400

    test

---
+++ PeerDownloadConnection.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: al
+ * Date: 7/22/13
+ * Time: 6:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PeerDownloadConnection extends Thread{
+    Socket peerDownloadSocket;
+    boolean active;
+
+
+    public PeerDownloadConnection(String ipAddress, int port) throws IOException {
+
+    }
+
+    public void run() {
+        while(active) {
+
+        }
+        try {
+            peerDownloadSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public void close() {
+        active = false;
+    }
+}
---
+++ PeerUploadConnection.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: al
+ * Date: 7/22/13
+ * Time: 6:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PeerUploadConnection extends Thread{
+    ServerSocket peerUploadSocket;
+    boolean active;
+
+    public PeerUploadConnection(String ipAddress, int port) throws IOException {
+
+    }
+
+    public void run() {
+        while(active) {
+
+        }
+        try {
+            peerUploadSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public void close() {
+        active = false;
+    }
+}
