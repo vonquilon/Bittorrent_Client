3e5936e3c6a1df489d9cff9a524d8e88a0e9af59
vonquilon <von.quilon@gmail.com>
2013-07-24 12:03:54 -0400

    Added some comments. This class holds the byte[] file that's shared by threads

--- FileManager.java
+++ FileManager.java
@@ -15,6 +15,15 @@ public class FileManager {
 		
 	}
 	
+	/**
+     * Method that assembles the piece into the file.
+     *
+     * @param index     When multiplied by pieceSize, indicates the location
+     *                  of the piece in the file
+     * @param piece     Piece to be assembled into file
+     * @param file      Where piece is assembled into
+     * @param pieceSize
+     */
 	public synchronized void putPieceInFile(int index, byte[] piece, int pieceSize) {
 
 		//pieceSize*index = byte offset in file
