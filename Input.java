2ddafcb3db2ad3630ddb5439a8be0dd660ff772f
vonquilon <von.quilon@gmail.com>
2013-07-30 13:22:29 -0400

    added queues

--- Input.java
+++ Input.java
@@ -1,15 +1,57 @@
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 
 public class Input extends Thread {
 
 	private InputStream fromPeer;
+	private boolean stopped = false;
+	private static ArrayList<byte[]> inputQueue;
+	private static int messageLength = 4;
 	
 	public Input(InputStream fromPeer) {
 		this.fromPeer = fromPeer;
+		inputQueue = new ArrayList<byte[]>();
 	}
 	
 	public void run() {
-		
+		while(!stopped) {
+			try {
+				int length = fromPeer.available();
+				if(length != 0) {
+					while((length = fromPeer.available()) < messageLength) {
+						byte[] message = new byte[length];
+						fromPeer.read(message);
+						addToInputQueue(message);
+					}
+				}
+			} catch (IOException e) {
+				System.out.println("Unable to send");
+			}
+			if(inputQueueSize() != 0) {
+				
+			}
+		}
 	}
+	
+	private static synchronized void addToInputQueue(byte[] input) {
+    	inputQueue.add(input);
+    }
+    
+    public static synchronized byte[] getFromInputQueue() {
+    	return inputQueue.remove(0);
+    }
+    
+    private static synchronized int inputQueueSize() {
+    	return inputQueue.size();
+    }
+    
+    public static synchronized void setMessageLength(int length) {
+    	messageLength = length;
+    }
+    
+    public void halt() {
+    	stopped = true;
+    }
 }
--- Output.java
+++ Output.java
@@ -1,4 +1,44 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
 
 public class Output extends Thread {
-
+	
+	private OutputStream toPeer;
+	private boolean stopped  = false;
+	private static ArrayList<byte[]> outputQueue;
+	
+	public Output(OutputStream toPeer) {
+		this.toPeer = toPeer;
+		outputQueue = new ArrayList<byte[]>();
+	}
+	
+	public void run() {
+		while(!stopped) {
+			if(outputQueueSize() != 0) {
+				try {
+					toPeer.write(getFromOutputQueue());
+				} catch (IOException e) {
+					System.out.println("Unable to send");
+				}
+			}//end if
+		}//end while
+	}
+	
+	public static synchronized void addToOutputQueue(byte[] output) {
+    	outputQueue.add(output);
+    }
+    
+    private static synchronized byte[] getFromOutputQueue() {
+    	return outputQueue.remove(0);
+    }
+    
+    private static synchronized int outputQueueSize() {
+    	return outputQueue.size();
+    }
+	
+	public void halt() {
+		stopped = true;
+	}
 }
