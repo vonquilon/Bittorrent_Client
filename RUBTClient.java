423c34f8b05f86050d27179c08f49d2718d59846
vonquilon <von.quilon@gmail.com>
2013-08-01 13:28:28 -0400

    UPDATED - new GUI system

--- RUBTClient.java
+++ RUBTClient.java
@@ -1,106 +1,177 @@
-import java.io.FileOutputStream;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 /**
  * RUBTClient is a simple BitTorrent client that parses a torrent file
- * and downloads an image file from one peer.
+ * and downloads a file specified in the torrent meta info.
  * 
  * @authors Von Kenneth Quilon & Alex Loh
- * @date 07/12/2013
- * @version 1.0
+ * @date 08/01/2013
+ * @version 1.2
+ * 
+ * UPDATES:
+ * -New GUI
+ * -Now picks a port between 6881 and 6889
  */
 public class RUBTClient {
+	
+	private JFrame frame;
+	private JTextArea textArea;
+	private JPanel bottomPanel;
+	private JButton start, pause, exit;
+	private String torrentName, fileName;
+	
+	/**
+	 * Creates a RUBTClient with the specified torrent file name and the
+	 * name of the file to be saved into the hard disk.
+	 * 
+	 * @param torrentName - the torrent file name
+	 * @param fileName - the name of the file to be saved
+	 */
+	public RUBTClient(String torrentName, String fileName) {
+		frame = new JFrame("RUBTClient");
+		textArea = new JTextArea(10, 50);
+		bottomPanel = new JPanel(new BorderLayout());
+		createStartButton();
+		createPauseButton();
+		createExitButton();
+		
+		this.torrentName = torrentName;
+		this.fileName = fileName;
+	}
 
 	/**
-	 * CONSTRUCTOR
+	 * Creates a start button that when pressed will start the process of downloading a file
+	 * from a torrent.
 	 */
-    public RUBTClient() {
-
+	private void createStartButton() {
+    	start = new JButton("Start"); 
+    	start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	try {
+            		ClientInfo.setPort();
+            		System.out.println("Using port: " + ClientInfo.port + "\n");
+            		TorrentInfo torrentInfo = TorrentParser.parseTorrent(torrentName);
+            	} catch (NoSuchElementException exception) {
+            		System.err.println(exception.getMessage() + "\n");
+            	}
+            }
+        });
     }
-
-    /**
-     * This method returns a new TorrentFile class.
-     * 
-     * @param torrentFilename Name of torrent file
-     * @return TorrentFile
-     */
-    public static TorrentFile readInTorrentFile(String torrentFilename) {
-    	
-        return new TorrentFile(torrentFilename);
-        
+	
+	/**
+	 * Creates a pause button that when pressed will pause the client's progress.
+	 */
+	private void createPauseButton() {
+    	pause = new JButton("Pause");
+    	pause.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+            	
+            }
+        });
     }
-
-    /**
-     * This method returns an Object[] that contains a Map,
-     * which is filled with torrent information, and a ByteBuffer
-     * of the info hash.
-     * 
-     * @param torrentFile
-     * @return Object[] Holds torrent information
-     */
-    public static Object[] parseTorrentFile(TorrentFile torrentFile) {
-    	
-        return torrentFile.parseTorrent();
-        
+	
+	/**
+	 * Creates an exit button that when pressed will close all alive threads, close all
+	 * open connections, closes frame processes, and exits the program.
+	 */
+	private void createExitButton() {
+    	exit = new JButton("Exit");
+    	exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed( ActionEvent e ) {
+            	frame.dispose();
+            	System.exit(0);
+            }
+        });
     }
-
-    /**
-     * This method contacts the tracker and obtains a response from
-     * the tracker.
-     * 
-     * @param torrentInfo Contains torrent information
-     * @param torrentFile
-     * @param peerID 
-     * @return byte[] Tracker response in bytes
-     */
-    public static byte[] contactTracker(Object[] torrentInfo, TorrentFile torrentFile, byte[] peerID) {
-    	
-        TrackerConnection trackerConnection = new TrackerConnection();
-        return trackerConnection.getTrackerResponse(torrentInfo, torrentFile, peerID);
-        
-    }
-
-    /**
-     * Parses the torrent file, gets tracker response, obtain list of peers,
-     * downloads file, and writes file to the disk.
-     * 
-     * @param args args[0]: torrent file
-     * 			   args[1]: name of file to save data to
-     */
-    public static void main(String args[]) {
-    	
-        try {
-            byte[] peerID = Functions.generatePeerID();
-            TorrentFile torrentFile = readInTorrentFile(args[0]);
-
-            Object[] torrentInfo = parseTorrentFile(torrentFile);
-            byte[] trackerResponse = contactTracker(torrentInfo, torrentFile, peerID);
-
-            ArrayList<String> peers = TrackerConnection.getPeersFromTrackerResponse(trackerResponse);
-            byte[] file = PeerConnection.getFileFromPeer(peers, torrentFile, peerID);
-
-            saveDownloadedFile(args[1], file);
-        } catch (Exception e) {
-        	System.err.println("Could not save to " + args[1]);
-        }
-        
-    }
-
-    /**
-     * Private helper method that saves the downloaded data into the disk.
-     * 
-     * @param downloadedFilename Name of file to save to
-     * @param fileData Data to be saved in bytes
-     */
-    private static void saveDownloadedFile(String downloadedFilename, byte[] fileData) {
-    	
-        try (FileOutputStream fileWriter = new FileOutputStream(downloadedFilename)) {
-            fileWriter.write(fileData);
-            System.out.println("Saved file as " + downloadedFilename);
-        } catch (IOException e) {
-            System.err.println("Could not save to " + downloadedFilename);
-        }
-        
-    }
+	
+	/**
+	 * Organizes and builds the GUI. System.out and System.err are rerouted to the GUI's JTextArea.
+	 */
+	public void build() {
+		PrintStream printStream = new PrintStream(new TextAreaOutputStream());
+		System.setOut(printStream);
+		System.setErr(printStream);
+		
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+		JScrollPane scroll = new JScrollPane(textArea);
+		frame.getContentPane().add(scroll, BorderLayout.CENTER);
+		
+		textArea.setEditable(false);
+		textArea.setBackground(Color.black);
+		textArea.setForeground(Color.white);
+		textArea.append("/**\n");
+		textArea.append(" *  Welcome to RUBTClient!\n");
+		textArea.append(" */\n\n");
+		
+		start.setPreferredSize(new Dimension(200, 20));
+		pause.setPreferredSize(new Dimension(200, 20));
+		exit.setPreferredSize(new Dimension(200, 20));
+		
+		bottomPanel.add(start, BorderLayout.LINE_START);
+		bottomPanel.add(pause, BorderLayout.CENTER);
+		bottomPanel.add(exit, BorderLayout.LINE_END);
+		frame.getContentPane().add(bottomPanel, BorderLayout.PAGE_END);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	/**
+	 * This class implements a TextAreaOutputStream that reroutes the OutputStream
+	 * to the GUI's JTextArea.
+	 * 
+	 * @author Von Kenneth Quilon
+	 * @date 08/01/2013
+	 * @version 1.0
+	 */
+	private class TextAreaOutputStream extends OutputStream {
+		
+		/**
+		 * Creates a TextAreaOutputStream.
+		 */
+		public TextAreaOutputStream() {
+			
+		}
+		
+		/**
+		 * Overridden write method from the OutputStream class. Reroutes the OutputStream
+		 * to this GUI's JTextArea.
+		 * 
+		 * @param b - the data to be printed
+		 */
+		@Override
+		public void write(int b) throws IOException {
+			textArea.append(String.valueOf((char) b));
+			textArea.setCaretPosition(textArea.getDocument().getLength());
+		}
+	}
+	
+	/**
+	 * Runs the RUBTClient.
+	 * 
+	 * @param args - args[0]: the torrent file name
+	 * 				 args[1]: the file name to save to
+	 */
+	public static void main(String[] args) {
+		RUBTClient client = new RUBTClient(args[0], args[1]);
+		client.build();
+	}
 }
