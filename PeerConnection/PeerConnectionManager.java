e2cd969ea97d3a3a428ea6f7f3fe5233c6055682
admin <padtherays@yahoo.com>
2013-08-10 14:03:36 -0400

    new branch

--- PeerConnection/PeerConnectionManager.java
+++ PeerConnection/PeerConnectionManager.java
@@ -1,5 +1,6 @@
 package PeerConnection;
 
+
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -12,7 +13,7 @@ import java.util.ArrayList;
  */
 public class PeerConnectionManager extends Thread{
     ArrayList<PeerConnection> activeConnections;
-
+    TorrentInfo info;
     public void run() {
 
     }
